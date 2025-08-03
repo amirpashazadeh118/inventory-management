781eec247ddca9c9ac100b43f3395aaa2affd356
Amir PashaZadeh <amirpa@systemgroup.net>
2025-08-03 04:31:08 +0330

    ADD: mail Service - accept order for admin API

--- inventoryRoutes.js
+++ inventoryRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { queryDb, sql } = require("./db.js");
 const { authorizeRoles, authenticateJWT } = require("./userRoutes.js");
+const nodemailer = require('nodemailer');
 
 module.exports = router;
 
@@ -165,24 +166,86 @@ async function createOrder(req, res) {
 }
 
 async function acceptOrder(req, res) {
-  const { orderID } = req.body;
+  const { orderID, partID, state , count, partName} = req.body;
 
-  // try {
-  //   const result = await queryDb(
-  //     `INSERT INTO [Order] (Description, CreateAt, TotalCost, UserRef)
-  //     VALUES (@Description, getDate(), @TotalCost, @UserRef, 1, @PartID, @Count, @Cost)`,
-  //     [
-  //       { name: "Description", type: sql.NVarChar, value: Description },
-  //       { name: "TotalCost", type: sql.BigInt, value: TotalCost },
-  //       { name: "UserRef", type: sql.BigInt, value: user.id },
-  //       { name: "PartID", type: sql.BigInt, value: PartID },
-  //       { name: "Cost", type: sql.BigInt, value: Cost },
-  //       { name: "Count", type: sql.BigInt, value: Count }
-  //     ]
-  //   );
-  //   const orderId = result.recordset[0].orderID;
-  //   res.status(200).send({ id: orderId }); 
-  // } catch (err) {
-  //   res.status(500).send(err.message);
-  // }
+  if(state === 3){// reject
+    try {
+      const result = await queryDb(
+        `UPDATE [Order] SET State = 3 where OrderID = @OrderID`,
+        [
+          { name: "OrderID", type: sql.BigInt, value: orderID }
+        ]
+      );
+      sendSMS("Order Rejected",`order ${orderID} rejected`);
+      res.status(200).send({ id: orderId }); 
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  } else if (state === 2) { //accept
+    try {
+      const part = await queryDb(
+        `Select Remaining FROM [Part] where PartID = @PartID`,
+        [
+          { name: "PartID", type: sql.BigInt, value: partID }
+        ]
+      );
+      var Remaining = part.recordset[0];
+      var newRemaining = Remaining - count;
+      if(newRemaining < 0){
+        return res.status(400).send("There is not enough goods in inventory"); 
+      }
+      
+      const result = await queryDb(
+        `Update [Part] set remaining = @newRemaining where PartID = @PartID;
+         UPDATE [Order] SET State = 2 where OrderID = @OrderID `,
+        [
+          { name: "PartID", type: sql.BigInt, value: partID },
+          { name: "newRemaining", type: sql.Int, value: newRemaining },
+          { name: "OrderID", type: sql.Int, value: orderID }
+        ]
+      );
+
+      sendSMS("Order Accepted",`order ${orderID} accepted`);
+
+      const partRemaining = await queryDb(
+        `Select Remaining FROM [Part] where PartID = @PartID`,
+        [
+          { name: "PartID", type: sql.BigInt, value: partID }
+        ]
+      );
+      var RemainingNow = part.partRemaining[0];
+      if(RemainingNow < 10){
+        sendSMS("LOW Part Remaining",`there is just RemainingNow of ${partName} in ${inventory}`);
+      }
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  } else {
+    res.status(500).send("Invalid State");
+  }
+}
+
+async function sendSMS(subject, text) {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'takhfifano@gmail.com',
+        pass: 'najbvnnfhiisgwjj', // Use App Password, NOT your actual password
+      },
+    });
+
+    // Set up mail options
+    const mailOptions = {
+      from: '"takhfifano" <takhfifano@gmail.com>',
+      to: "amiramjad2002@gmail.com", // TODO email of admin or ...
+      subject: subject,
+      text: text,
+    };
+
+    // Send mail
+    const info = await transporter.sendMail(mailOptions);
+  } catch (err) {
+    return res.status(500).send(`Failed to send email: ${err.message}`);
+  }
 }

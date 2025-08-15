b65b157f162d445546262cdc14d7ee0191584c22
Hamed Tabatabaeifar <hamedtabafar@gmail.com>
2025-08-15 22:28:32 +0330

    implementing "Add Category"

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,14 +3,15 @@ import {LoginComponent} from './pages/home2/home2.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component'
 import { AddgoodComponent } from './pages/addgood/addgood.component';
+import { AddcategoryComponent } from './pages/addcategory/addcategory.component';
 
 export const routes: Routes = [
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
       { path: 'dashboard', component: DashboardComponent },
       { path: 'products/new', component: AddgoodComponent },
+      { path: 'categories/new', component: AddcategoryComponent}
 
-      // { path: '/products/new', component: DashboardComponent },
       // { path: '/orders/new', component: DashboardComponent },
       // { path: '/users/edit', component: DashboardComponent },
       // { path: '/orders', component: DashboardComponent },
